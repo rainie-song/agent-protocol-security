2f77ef65e8d77f5698c6bd05946594f6bc515506
fcapub <221670706+fcapub@users.noreply.github.com>
2025-07-19 23:13:21 +0800

    Update keys_generator.py

--- keys_generator.py
+++ keys_generator.py
@@ -20,3 +20,29 @@ def generate_raw_keys():
     print ("Public key error:", e)
     
   return raw_public_key, raw_private_key
+
+def sign_rand(raw_secret_key_str: str, rand: int):
+  sk_raw = bytes.fromhex(raw_secret_key_str)
+  # Get secret key using raw secret key bytes
+  secret_key = Ed25519Privatekey.from_private_bytes(sk_raw)
+  # encode rand
+  encoded_rand = bytes(rand)
+  # sign rand with secret key
+  signed_rand = secret_key.sign(encoded_rand)
+  
+  return signed_rand
+
+def verify_rand(raw_public_key_str: str, rand: int, signed_rand: bytes):
+  # get raw public key from public key string
+  pk_raw = bytes.fromhex(raw_public_key_str)
+  # Get public key using raw public key bytes
+  public_key = Ed25519Publickey.from_public_bytes(pk_raw)
+  # encode rand
+  encoded_rand = bytes(rand)
+  # verify rand with signed rand
+  try:
+    public_key.verify(signed_rand, encoded_rand)
+  except InvalidSignature:
+    return False # invalid
+  
+  return True # valid
